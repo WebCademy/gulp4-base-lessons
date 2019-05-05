c3ecf5ca0d77c2370ce63541a73b2c32268363ab
Yurij <yurij.kharin@gmail.com>
2019-05-05 20:36:05 +0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var less = require('gulp-less');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
