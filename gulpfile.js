b27941d030ab92fb6a8e5791b0718f7458f84717
Yurij <yurij.kharin@gmail.com>
2019-05-05 19:56:19 +0300

    Second

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,18 @@
 var gulp = require('gulp');
 
-gulp.task('first', function(done){
+function gulpFirst (done) {
 	console.log('Самая первая задача! Это задача first!');
 	done();
-});
+}
 
-gulp.task('second', function(done){
+function gulpSecond (done) {
 	console.log('Вторая задача! Это задача second!');
 	done();
-});
+}
 
-gulp.task('third', function(done){
+function gulpThird (done) {
 	console.log('Третья задача! Это задача third!');
 	done();
-});
+}
 
-
-// gulp.task('default', function(){
-// 	console.log('Дефолтная задаа по умолчанию! Это задача default!');
-// });
-
-gulp.task('default', gulp.series('first', 'second', 'third'));
-// gulp.task('default', gulp.parallel('first', 'second', 'third'));
+gulp.task('default', gulp.series(gulpFirst, gulpSecond, gulpThird));
