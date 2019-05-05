c5dfbca296cbdbd6e861ccd721d9d26ea62c626d
Yurij <yurij.kharin@gmail.com>
2019-05-05 20:31:17 +0300

    Fourth

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
 
-gulp.task('server', function() {
-    browserSync.init({
-    	server: { baseDir: './app/'}
-    });
-    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    gulp.watch('./app/**/*.css').on('change', browserSync.reload);
+gulp.task('less', function() {
+	return gulp.src('./app/less/main.less')
+		.pipe(less())
+		.pipe(gulp.dest('./app/css/'))
+		.pipe(browserSync.stream())
 });
 
-gulp.task('default', gulp.series('server'));
+gulp.task('server', function() {
+	browserSync.init({
+		server: { baseDir: './app/'}
+	});
+	gulp.watch('./app/**/*.html').on('change', browserSync.reload);
+	gulp.watch('./app/less/**/*.less', gulp.series('less') );
+});
+
+gulp.task('default', gulp.series('less', 'server'));
